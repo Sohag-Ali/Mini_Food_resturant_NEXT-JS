a9c3ea0386694c10b24609b0f8cdf29149bd2edb
sohag142 <sohag2879@gmail.com>
2026-06-08 18:10:04 +0600

    metadata added

--- src/app/foods/[id]/page.jsx
+++ src/app/foods/[id]/page.jsx
@@ -4,6 +4,22 @@ export function generateStaticParams() {
   return [{ id: "52898" }, { id: "52955" }, { id: "52926" }];
 }
 
+
+export async function generateMetadata({ params }) {
+  const { id } = await params;
+  const res = await fetch(
+    `https://taxi-kitchen-api.vercel.app/api/v1/foods/${id}`
+  );
+
+  const {details={}} = await res.json();
+
+  return {
+    title: details.title,
+    description: `Best food review website in Bangladesh. Read reviews of ${details.title} and many more foods.`,
+  }
+}
+
+
 const getSingleFood = async (id) => {
   const res = await fetch(
     `https://taxi-kitchen-api.vercel.app/api/v1/foods/${id}`
--- src/app/foods/page.jsx
+++ src/app/foods/page.jsx
@@ -14,6 +14,14 @@ const getFoods = async (search) => {
   await new Promise((resolve) => setTimeout(resolve, 1000));
   return data.foods || [];
 };
+
+
+export const metadata = {
+  title: "All Foods",
+  description: "Best food review website in Bangladesh",
+};
+
+
 const FoodsPage = async ({ searchParams }) => {
   const { search = "" } = await searchParams;
 
